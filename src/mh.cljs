65a033ea3d559e7d6bbcc3d401f6e720875bead3
richie <6143833+rgkirch@users.noreply.github.com>
2022-06-25 17:49:49 -0400

    added comment

--- src/mh.cljs
+++ src/mh.cljs
@@ -26,34 +26,35 @@
                      (fn unmount [] (.removeEventListener dom-node event-name send!))))))
 
 
+;; this code works
+(comment
+  ((mi/reactor
+    (let [db (atom nil)
+          field (observe-ref db)
+          valid? (mi/signal! (mi/ap (seq (mi/?< field))))
+          button-node (doto (gdom/createDom "input" (clj->js {:type "button"
+                                                              :value "Submit"}))
+                        (as-> node (mi/stream!
+                                    (mi/ap
+                                     (let [disabled (not (mi/?< valid?))]
+                                       (gdom/setProperties node (clj->js {:disabled disabled})))))))
+          input-node (gdom/createDom "input" (clj->js {:type "text"}))]
+      (mi/stream!
+       (mi/ap (let [x (mi/?< (observe-event button-node "click"))]
+                (println (.-pointerType x) "clicked: text is" (mi/?< (mi/eduction (take 1) field))))))
+      (mi/stream!
+       (mi/ap
+        (let [x (mi/?< (observe-event input-node "input"))]
+          (reset! db (.. x -target -value)))))
+      (gdom/replaceNode
+       (gdom/createDom "div" (clj->js {:id "app-container"})
+                       (gdom/createDom "div" nil input-node)
+                       (gdom/createDom "div" nil button-node))
+       (gdom/getElement "app-container"))))
+   prn prn))
 
-((mi/reactor
-  (let [db (atom nil)
-        field (observe-ref db)
-        valid? (mi/signal! (mi/ap (seq (mi/?< field))))
-        button-node (doto (gdom/createDom "input" (clj->js {:type "button"
-                                                            :value "Submit"}))
-                      (as-> node (mi/stream!
-                                  (mi/ap
-                                   (let [disabled (not (mi/?< valid?))]
-                                     (gdom/setProperties node (clj->js {:disabled disabled})))))))
-        input-node (gdom/createDom "input" (clj->js {:type "text"}))]
-    (mi/stream!
-     (mi/ap (let [x (mi/?< (observe-event button-node "click"))]
-              (println (.-pointerType x) "clicked: text is" (mi/?< (mi/eduction (take 1) field))))))
-    (mi/stream!
-     (mi/ap
-      (let [x (mi/?< (observe-event input-node "input"))]
-        (reset! db (.. x -target -value)))))
-    (gdom/replaceNode
-     (gdom/createDom "div" (clj->js {:id "app-container"})
-                     (gdom/createDom "div" nil input-node)
-                     (gdom/createDom "div" nil button-node))
-     (gdom/getElement "app-container"))))
- prn prn)
 
-
-
+;; this code is broken
 (defn ^:dev/after-load start! []
   ((mi/reactor
   (let [db (atom nil)
@@ -81,3 +82,5 @@
                      (gdom/createDom "div" nil button-node))
      (gdom/getElement "app-container"))))
    prn prn))
+
+;; mouse clicked: text is #object[missionary.impl.Ambiguous.Branch]
