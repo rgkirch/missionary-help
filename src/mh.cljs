4ba0cdc7598804a91b7056d17d852ae464a454b4
richie <6143833+rgkirch@users.noreply.github.com>
2022-06-25 17:42:15 -0400

    first

---
+++ src/mh.cljs
@@ -0,0 +1,83 @@
+(ns mh
+  (:require
+   [clojure.string :as string]
+   [cljs.pprint :refer [pprint]]
+   [goog.dom :as gdom]
+   [missionary.core :as mi]))
+
+(enable-console-print!)
+
+(defn observe-ref
+  ([r]
+   (observe-ref r nil))
+  ([r initial]
+   (->> (mi/observe (fn mount [send!]
+                      (let [id (random-uuid)]
+                        (add-watch r id (fn [k r o n] (send! n)))
+                        (send! @r)
+                        (fn unmount [] (remove-watch r id)))))
+        (mi/reductions {} initial)
+        (mi/relieve {}))))
+
+(defn observe-event
+  [dom-node event-name]
+  (->> (mi/observe (fn mount [send!]
+                     (.addEventListener dom-node event-name send!)
+                     (fn unmount [] (.removeEventListener dom-node event-name send!))))))
+
+
+
+((mi/reactor
+  (let [db (atom nil)
+        field (observe-ref db)
+        valid? (mi/signal! (mi/ap (seq (mi/?< field))))
+        button-node (doto (gdom/createDom "input" (clj->js {:type "button"
+                                                            :value "Submit"}))
+                      (as-> node (mi/stream!
+                                  (mi/ap
+                                   (let [disabled (not (mi/?< valid?))]
+                                     (gdom/setProperties node (clj->js {:disabled disabled})))))))
+        input-node (gdom/createDom "input" (clj->js {:type "text"}))]
+    (mi/stream!
+     (mi/ap (let [x (mi/?< (observe-event button-node "click"))]
+              (println (.-pointerType x) "clicked: text is" (mi/?< (mi/eduction (take 1) field))))))
+    (mi/stream!
+     (mi/ap
+      (let [x (mi/?< (observe-event input-node "input"))]
+        (reset! db (.. x -target -value)))))
+    (gdom/replaceNode
+     (gdom/createDom "div" (clj->js {:id "app-container"})
+                     (gdom/createDom "div" nil input-node)
+                     (gdom/createDom "div" nil button-node))
+     (gdom/getElement "app-container"))))
+ prn prn)
+
+
+
+(defn ^:dev/after-load start! []
+  ((mi/reactor
+  (let [db (atom nil)
+        field (observe-ref db)
+        valid? (mi/signal! (mi/ap (seq (mi/?< field))))
+        button-node (doto (gdom/createDom "input" (clj->js {:type "button"
+                                                            :value "Submit"}))
+                      (as-> node (mi/stream!
+                                  (mi/ap
+                                   (let [disabled (not (mi/?< valid?))]
+                                     (gdom/setProperties node (clj->js {:disabled disabled})))))))
+        input-node (gdom/createDom "input" (clj->js {:type "text"}))]
+    (mi/stream!
+     (mi/ap
+      (let [func (fn [x]
+                   (println (.-pointerType x) "clicked: text is" (mi/?< (mi/eduction (take 1) field))))]
+        (func (mi/?< (observe-event button-node "click"))))))
+    (mi/stream!
+     (mi/ap
+      (let [x (mi/?< (observe-event input-node "input"))]
+        (reset! db (.. x -target -value)))))
+    (gdom/replaceNode
+     (gdom/createDom "div" (clj->js {:id "app-container"})
+                     (gdom/createDom "div" nil input-node)
+                     (gdom/createDom "div" nil button-node))
+     (gdom/getElement "app-container"))))
+   prn prn))
